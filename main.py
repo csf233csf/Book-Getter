64eb61e01b899cabdd65241aa5e000f3026a33ed
Kano <56235101+csf233csf@users.noreply.github.com>
2023-01-02 02:57:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.biquqq.com/0_1/1.html'
-
-req = requests.get(url)
-
-req.encoding = 'gbk'
-
-soup = BeautifulSoup(req.text,"html.parser")
-
-print(soup.title.text)
+# Yet to Write
